9492ed8c2d369dd387ae80850c0a6e330e69687b
JeongDeokHo <ekdj98@gmail.com>
2022-02-17 15:47:31 +0900

    c

--- Project-Clone-Uber/Controller/LoginController.swift
+++ Project-Clone-Uber/Controller/LoginController.swift
@@ -30,9 +30,8 @@ class LoginController: UIViewController {
         view.backgroundColor = UIColor(displayP3Red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
         
         view.addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false //false로 해주어야 오토레이아웃을 내가 조정할수 있음
-        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //중앙정렬을 뷰의 중앙정렬에 맞춰 액티브
-        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true //상부 앵커를 뷰의 상부에 맞춰 액티브
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 20)
+        titleLabel.centerX(inView: view)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle { //시간이나 와이파이같은 스테이터스 바의 스타일을 바꾸는 코드
