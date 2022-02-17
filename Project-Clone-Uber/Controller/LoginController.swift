a0964841cd78405e4d528913c95cd21cb9aaada5
JeongDeokHo <ekdj98@gmail.com>
2022-02-17 16:56:13 +0900

    commit

--- Project-Clone-Uber/Controller/LoginController.swift
+++ Project-Clone-Uber/Controller/LoginController.swift
@@ -11,6 +11,8 @@ class LoginController: UIViewController {
     
     //MARK: - Properties
     
+    
+    //우버 레이블
     private let titleLabel: UILabel = { //레이블을 만들기
         let label = UILabel()
         label.text = "UBER"
@@ -18,6 +20,35 @@ class LoginController: UIViewController {
         label.textColor = UIColor(white: 1, alpha: 0.8)
         return label
     }()
+    
+    
+    
+    
+    
+    //로그인 - 이메일
+    private lazy var emailContrainerView: UIView = {
+        return UIView().inputContainerView(image: UIImage(named: "ic_mail_outline_white_2x")!, textField: emailTextField)
+    }()
+    
+    private let emailTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Email", isSecureTextEntry: false)
+    }()
+    
+    
+    //로그인 - 비밀번호
+    private lazy var passwordContrainerView: UIView = {
+        return UIView().inputContainerView(image: UIImage(named: "ic_lock_outline_white_2x")!, textField: passwordTextField)
+    }()
+    
+    private let passwordTextField: UITextField = {
+        return UITextField().textField(withPlaceholder: "Password", isSecureTextEntry: true)
+    }()
+    
+    
+    
+    
+    
+    
     
     //MARK: - Lifecycle
 
@@ -32,6 +63,17 @@ class LoginController: UIViewController {
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 20)
         titleLabel.centerX(inView: view)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContrainerView, passwordContrainerView])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        
+        
+        
+        view.addSubview(emailContrainerView)
+        emailContrainerView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
+        view.addSubview(passwordContrainerView)
+        passwordContrainerView.anchor(top: emailContrainerView.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16, height: 50)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle { //시간이나 와이파이같은 스테이터스 바의 스타일을 바꾸는 코드
--- Project-Clone-Uber/Utils/Extension.swift
+++ Project-Clone-Uber/Utils/Extension.swift
@@ -8,6 +8,27 @@
 import UIKit
 
 extension UIView {
+    
+    func inputContainerView(image: UIImage, textField: UITextField) -> UIView {
+        let view = UIView()
+        let imageView = UIImageView()
+        imageView.image = image
+        imageView.alpha = 0.87
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8,width: 24, height: 24)
+
+        view.addSubview(textField)
+        textField.anchor(left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, paddingBottom: 8)
+        textField.centerY(inView: view)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8,height: 0.75)
+        return view
+    }
+    
     func anchor(top: NSLayoutYAxisAnchor? = nil,left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat = 0, paddingLeft: CGFloat = 0, paddingBottom: CGFloat = 0, paddingRight: CGFloat = 0, width: CGFloat? = nil, height: CGFloat? = nil) {
         
         translatesAutoresizingMaskIntoConstraints = false
@@ -39,5 +60,22 @@ extension UIView {
     
     func centerX(inView view: UIView) {
         centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func centerY(inView view: UIView) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
+
+extension UITextField {
+    func textField(withPlaceholder placeholder: String, isSecureTextEntry: Bool) -> UITextField {
+        let tf = UITextField()
+        tf.borderStyle = .none //테두리 스타일 없음
+        tf.font = UIFont.systemFont(ofSize: 16) //폰트 설정
+        tf.textColor = .white //글자 색깔
+        tf.keyboardAppearance = .dark //키보드 색깔
+        tf.isSecureTextEntry = isSecureTextEntry //키보드 보안 설정(비밀번호 입력시에)
+        tf.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return tf
     }
 }
