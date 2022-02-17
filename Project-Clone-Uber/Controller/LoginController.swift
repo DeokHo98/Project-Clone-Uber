f5d1dd0294d61cce3277699243271be7544ef93c
JeongDeokHo <ekdj98@gmail.com>
2022-02-17 14:37:13 +0900

    c

---
+++ Project-Clone-Uber/Controller/LoginController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginController.swift
+//  Project-Clone-Uber
+//
+//  Created by 정덕호 on 2022/02/17.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     //askdsakdaskdkasdkasdk
+    //asdkaskdaskdaksdkasdkk
+    }
+}
